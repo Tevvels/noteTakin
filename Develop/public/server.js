3bd3c50d51730871da0af4a6d7d65a4aec59cb26
Christopher Watkins <chrisbwatkins@gmail.com>
2021-02-22 19:02:51 -0700

    working for the most part

--- Develop/public/server.js
+++ Develop/public/server.js
@@ -24,33 +24,22 @@ app.get('/api/notes',(req,res) =>{
         })
     })
 
-    app.get('/api/notes/:note',(req,res) =>{
-        const chosen = req.params.note;
-        console.log(chosen);
-    })
-
     //cd develop/public
 
     const datatree =[];
-    datatree.push(datas)
+
 
 app.post('/api/notes', (req,res) =>{
-
-    console.log(datas)
-
      datatree.push(req.body);
-     res.json(true)
-    // console.log(datatree)
+
     fs.readFile('./assets/js/index.js','utf8',(err,data)=>{
         if(err){
-            throw err;
-        }
-        console.log((data.activeNote))
+        throw err;
+        }   
         fs.writeFile(paths,JSON.stringify(datatree),(err,data)=>{
             if(err){
                     throw err;
-            }
-           
+            }      
             console.log("something written");
         });
     })  
@@ -62,12 +51,11 @@ app.use("/assets",express.static(__dirname + '/assets'))
 
 
 
-app.get('/',(req,res)=> res.sendFile(path.join(__dirname,'index.html')));
 
 app.get('/notes', (req,res)=> res.sendFile(path.join(__dirname,'notes.html')));
 
 
 
-
+app.get('*',(req,res)=> res.sendFile(path.join(__dirname,'index.html')));
 
 app.listen(PORT,()=> console.log(`App listening on PORT ${PORT}`));
